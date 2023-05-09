e8f91e2587491662dfbff1dd78e3ec7f216fef5f
Suman Sinha <sinha1194@gmail.com>
2023-05-09 22:46:16 +0530

    Implementation of useIsMounted() Hook

---
+++ useIsMounted Hook/useIsMounted.js
@@ -0,0 +1,12 @@
+import React, {useRef, useEffect} from 'react';
+
+export function useIsMounted(): () => boolean {
+  const isMounted = useRef(false);
+  
+  useEffect(() => {
+    isMounted.current = true;
+    return () => {isMounted.current = false;}
+  }, []);
+
+  return () => isMounted.current;
+}
