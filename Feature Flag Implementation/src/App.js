e6d1d0d1e9eb1f76f65ef27b47b81c8cce04d57a
Suman Sinha <sinha1194@gmail.com>
2023-10-03 22:39:32 +0530

    Create App.js

---
+++ Feature Flag Implementation/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import FeatureFlagComponent from "./components/FeatureFlag";
+
+function App() {
+  return (
+    <>
+      <FeatureFlagComponent />
+    </>
+  );
+}
+
+export default App;
